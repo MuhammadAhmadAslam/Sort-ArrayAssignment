e2ac9a7b0b3c594fce3c3f4adc020575e05b1800
MuhammadAhmadAslam <prince1ahmed111@gmail.com>
2024-05-15 20:18:27 +0500

    Array Sort

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 let NumberArray = [1,5,6,22,10,20,11,32,12,7,4,190,221,280,6,8,10,20,77,55,0,5,10];
+let e1 = NumberArray.filter(NumberArray => NumberArray < 9)
+let e2 = NumberArray.filter(NumberArray => NumberArray >= 9 && NumberArray <= 99);
+let e3 = NumberArray.filter(NumberArray => NumberArray > 99 && NumberArray <= 999)
+let e4 = NumberArray.filter(NumberArray => NumberArray > 999 && NumberArray <= 9999)
+let e5 = NumberArray.filter(NumberArray => NumberArray > 9999 && NumberArray <= 99999)
+let e6 = NumberArray.filter(NumberArray => NumberArray > 99999 && NumberArray <= 999999)
+e1 = e1.sort()
+e2 = e2.sort()
+e3 = e3.sort()
+e4 = e4.sort()
+e5 = e5.sort()
+e6 = e6.sort()
+let finalResult1 = [...e1 , ...e2 , ...e3 , ...e4 , ...e5 , ...e6]
+console.log(finalResult1);
 document.getElementById('s1').innerText = NumberArray
 let sorting = NumberArray.sort()
-console.log(sorting);
 let extract1 = []
 let extract2 = []
 let extract3 = []
@@ -21,11 +34,6 @@ for (let i = 0; i < NumberArray.length; i++) {
     }
 
 }
-
-console.log(extract1);
-console.log(extract2);
-console.log(extract3);
-
 let finalResult = [...extract1 , ...extract2 , ...extract3]
 document.getElementById('output').innerText = finalResult
 
